d92ec1ad134cc9b7f7f88502aa873b597f2f7d7e
CathyCorrea <cathy.correa@versionone.com>
2021-03-19 12:59:33 -0400

    Add TLS for powershell Invoke-WebRequest

--- build.ps1
+++ build.ps1
@@ -4,6 +4,7 @@ param(
 )
 
 function DownloadSetup {
+    [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
     $source = "https://raw.github.com/openAgile/psake-tools/master/setup.ps1"  
     Invoke-WebRequest -Uri $source  -UseBasicParsing -OutFile setup.ps1
 }
